67dec6a0b17d08621517453ab638e79a1ab9c94e
rsamec <roman.samec2@gmail.com>
2015-08-18 19:45:58 +0200

    remove default values from position editor

--- src/editors/PositionEditor.js
+++ src/editors/PositionEditor.js
@@ -26,8 +26,8 @@ export default class PositionEditor extends React.Component {
         this.setState({show:!this.state.show});
     }
     render() {
-        var value = _.extend(_.clone(defaultValues),this.props.value);
-        var text = _.reduce(value,function(result,value,key){ return result+= " " + value},"");
+        var value = this.props.value;
+        var text = _.reduce(value,function(result,value,key){ return result+= " " + !!value?value:'-'},"");
         return (
             <div className={this.state.show?'open':''}>
                 <span className="compoundToggle" onClick={this.toogle.bind(this)}>{text}</span>
